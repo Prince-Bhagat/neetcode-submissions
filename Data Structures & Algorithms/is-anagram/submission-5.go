5400aa54c1d501042c5980bd19a490decc14d6d6
Prince <bhagatprince66@gmail.com>
2026-08-07 16:26:52 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.go
@@ -0,0 +1,19 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t){
+        return false
+    }
+    frequency := [26]int{}
+    for _, sChar := range s{
+        frequency[sChar-'a']++
+    }
+    for _, tChar := range t{
+        frequency[tChar -'a']--
+    }
+
+    for _, freq := range frequency {
+        if freq != 0{
+            return false
+        }
+    }
+    return true
+}
