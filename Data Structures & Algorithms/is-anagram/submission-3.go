3746dd79348e03b7ee1f8fb8885dd9d7837dca23
Prince <bhagatprince66@gmail.com>
2026-08-07 16:09:34 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.go
@@ -0,0 +1,36 @@
+func isAnagram(s string, t string) bool {
+    hashMap := make(map[rune]int)
+    for _, char := range s{
+        _, isExist := hashMap[char]
+        if(isExist){
+            hashMap[char]++
+        }else{
+            hashMap[char] = 1
+        }
+    }
+    hashMap2 := make(map[rune]int)
+    for _, char := range t{
+        _, isExist := hashMap[char]
+        if !isExist{
+            return false
+        } 
+        
+        _, isExist = hashMap2[char]
+        if(isExist){
+            hashMap2[char]++
+        }else{
+            hashMap2[char] = 1
+        }
+    }
+    if (len(hashMap) != len(hashMap2)){
+        return false
+    }
+    for key, value := range hashMap{
+        if value != hashMap2[key]{
+            return false
+        }
+    }
+    
+    return true
+
+}
